2c82bd3aa202a19df5cfdaf706d590cc8802b376
Lukas Tarkowski <tarkowski14@gmail.com>
2021-04-19 13:41:12 +0200

    init

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+showNav = function(){
+    document.getElementById("navList").style.top = "20vh";
+    document.getElementById("menuBurgerShow").style.display = "none";
+}
+hideNav = function(){
+    document.getElementById("navList").style.top = "-7heroku0vh";
+    document.getElementById("menuBurgerShow").style.display = "block";
+}
